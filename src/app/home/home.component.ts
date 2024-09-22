279cdbfcd3a378820085ea50a2fe74ddfc3c129c
bao <bao-trn@outlook.fr>
2024-09-22 19:27:16 +0200

    changed time handling in home comp

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,99 +9,47 @@ import {MeetingParameters} from "../Interface/meeting-parameters";
 })
 export class HomeComponent {
 
-
-  public meetingDurationMinutes: number;
-  public meetingDurationSeconds: number;
-
-  public talkingDurationMinutes: number;
-  public talkingDurationSeconds: number;
-  public nbSpeakers: number;
-
+  public meetingDuration:number = 60; //TODO low values for testing purpose, to change
+  public talkingDuration:number = 15;
+  public nbSpeakers:number = 4;
   public overtime:string = 'always';
 
 
 
   constructor(private dataService:DataService<any>) {
-    this.meetingDurationMinutes = 1;
-    this.meetingDurationSeconds = 0;
-    this.talkingDurationMinutes = 0;
-    this.talkingDurationSeconds = 15;
-    this.nbSpeakers = 4;
   }
 
-  //TODO change all meeting to be computed with seconds within a service instead of minutes and seconds
+  //TODO change all meeting to be computed with seconds within a service and handle negatives
 
-  changeMeetingDuration(minutes:number, seconds:number) {
-    this.meetingDurationMinutes += minutes;
-    this.meetingDurationSeconds += seconds;
+  changeMeetingDuration(seconds:number) {
+    this.meetingDuration += seconds;
     this.adjustMeetingDurations()
-
-    //when seconds go above 60 increment minutes
-    if (this.meetingDurationSeconds >= 60) {
-      this.meetingDurationMinutes += Math.floor(this.meetingDurationSeconds / 60);
-      this.meetingDurationSeconds = this.meetingDurationSeconds % 60
-    }
-    //when seconds go below 0 decrement minutes
-    if (this.meetingDurationSeconds < 0) {
-      this.meetingDurationMinutes--;
-      this.meetingDurationSeconds = 60 + this.meetingDurationSeconds;
-    }
-    // when minutes go below 0 resets minutes and seconds
-    if (this.meetingDurationMinutes < 0) {
-      this.meetingDurationMinutes = 0;
-      this.meetingDurationSeconds = 0;
-    }
   }
 
-  changeTalkingDuration(minutes:number, seconds:number) {
-    this.talkingDurationMinutes += minutes;
-    this.talkingDurationSeconds += seconds;
+  changeTalkingDuration(seconds:number) {
+    this.talkingDuration += seconds;
     this.adjustTalkingDurations();
-
-    //when seconds go above 60 increment minutes
-    if (this.talkingDurationSeconds >= 60) {
-      this.talkingDurationMinutes += Math.floor(this.talkingDurationSeconds / 60);
-      this.talkingDurationSeconds = this.talkingDurationSeconds % 60
-    }
-    //when seconds go below 0 decrement minutes
-    if (this.talkingDurationSeconds < 0) {
-      this.talkingDurationMinutes--;
-      this.talkingDurationSeconds = 60 + this.talkingDurationSeconds;
-    }
-    // when minutes go below 0 resets minutes and seconds
-    if (this.talkingDurationMinutes < 0) {
-      this.talkingDurationMinutes = 0;
-      this.talkingDurationSeconds = 0;
-    }
   }
 
   //round durations to closest 30s bound and adjust the timers depending on the number of speakers
   adjustMeetingDurations() {
-    if ((this.meetingDurationSeconds % 30) != 0 && (this.meetingDurationSeconds - 30) < 0) {
-      this.meetingDurationSeconds = 0;
-    } else if ((this.meetingDurationSeconds % 30) != 0) {
-      this.meetingDurationSeconds = 30;
+    if ((this.meetingDuration % 30) != 0 && (this.meetingDuration - 30) < 0) {
+      this.meetingDuration = 0;
+    } else if ((this.meetingDuration % 30) != 0) {
+      this.meetingDuration = 30;
     }
 
-    const totalDurationSeconds = (this.meetingDurationMinutes * 60) + this.meetingDurationSeconds;
-    const talkingDuration = Math.floor(totalDurationSeconds / this.nbSpeakers);
-
-    this.talkingDurationMinutes = Math.floor(talkingDuration / 60);
-    this.talkingDurationSeconds = talkingDuration % 60;
+    this.talkingDuration = Math.floor(this.meetingDuration / this.nbSpeakers);
   }
 
   adjustTalkingDurations() {
-    if ((this.talkingDurationSeconds % 30) != 0 && (this.talkingDurationSeconds - 30) < 0) {
-      this.talkingDurationSeconds = 0;
-    } else if ((this.talkingDurationSeconds % 30) != 0) {
-      this.talkingDurationSeconds = 30;
+    if ((this.talkingDuration % 30) != 0 && (this.talkingDuration - 30) < 0) {
+      this.talkingDuration = 0;
+    } else if ((this.talkingDuration % 30) != 0) {
+      this.talkingDuration = 30;
     }
 
-    const totalDurationSeconds = (this.talkingDurationMinutes * 60) + this.talkingDurationSeconds;
-    const meetingDuration = Math.floor(totalDurationSeconds * this.nbSpeakers);
-
-    this.meetingDurationMinutes = Math.floor(meetingDuration / 60);
-    this.meetingDurationSeconds = meetingDuration % 60
+    this.meetingDuration = Math.floor(this.talkingDuration * this.nbSpeakers);
   }
 
   onNumberSpeakerChange(numberSpeakerEvent:number) {
@@ -110,8 +58,8 @@ export class HomeComponent {
   }
 
   sendMeetingData() {
-    const meetingDuration = (this.meetingDurationMinutes * 60) + this.meetingDurationSeconds;
-    const talkingDuration = (this.talkingDurationMinutes * 60) + this.talkingDurationSeconds;
+    const meetingDuration = this.meetingDuration;
+    const talkingDuration = this.talkingDuration;
     const nbSpeakers = this.nbSpeakers;
     const overtime = this.overtime;
     const meetingParams: MeetingParameters = {meetingDuration, talkingDuration, nbSpeakers, overtime};
